3513dd33b998ac31229128bdf66789d29e5f589a
Whaddup <simonwong163@gmail.com>
2018-05-03 17:22:03 -0400

    Thurs 3 May 2018 520pm

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,71 @@ false, false, false, false, false, false, false, false];
 let rideCymbals = [false, false, false, false, false, false, false, false,
 false, false, false, false, false, false, false, false];
 
+
+const transmogrifier = function(inputString){
+
+if (inputString = "kicks"){
+  return kicks;
+}
+else if (inputString = "snares"){
+  return snares;
+}
+else if (inputString = "hiHats"){
+  return hiHats;
+}
+else if (inputString = "rideCymbals"){
+  return rideCymbals
+}
+else {return}
+};
+
+
+const toggleDrum = function(instrumentString, indexNumber){
+let instrument = transmogrifier(instrumentString);
+
+if (instrument[indexNumber] === false){
+  instrument[indexNumber] = true
+}
+else {instrument[indexNumber] = false}
+}
+
+
+const clear = function(instrument){
+let instrument = transmogrifier(instrumentString);
+
+for (let x = 0; x < instrument.length; x++){
+   instrument[x] = false;
+     }
+     // return instrument
+}
+
+const invert = function(instrument){
+let instrument = transmogrifier(instrumentString);
+
+for (let x = 0; x < instrument.length; x++){
+    if (instrument[x] === false){
+      instrument[x] = true
+    }
+    else {instrument[x] = false}
+  }
+}
+
+
+
+/*
+// Drum Arrays
+let kicks = [false, false, false, false, false, false, false, false,
+false, false, false, false, false, false, false, false];
+
+let snares = [false, false, false, false, false, false, false, false,
+false, false, false, false, false, false, false, false];
+
+let hiHats = [false, false, false, false, false, false, false, false,
+false, false, false, false, false, false, false, false];
+
+let rideCymbals = [false, false, false, false, false, false, false, false,
+false, false, false, false, false, false, false, false];
+
 const toggleDrum = function(instrument, indexNumber){
   if (instrument[indexNumber] === false){
     instrument[indexNumber] = true
@@ -34,3 +99,4 @@ const invert = function(instrument){
     else {instrument[x] = false}
   }
 }
+*/
